8923b56f90d7bdb2a55b2ae71e8883cdae8afb10
Leon Omelan <rozpierog@gmail.com>
2024-10-22 15:00:24 +0200

    Add weather icon to CityCard

--- src/components/CityCard/index.tsx
+++ src/components/CityCard/index.tsx
@@ -3,13 +3,14 @@ import { View } from 'react-native';
 import { Card, Text, Icon, ActivityIndicator } from 'react-native-paper';
 import style from './style';
 import { useCurrentWeather } from '../../api/CurrentWeather';
+import mapIconsToCondition from '../../utils/mapIconsToCondition';
 
 type Props = {
     cityId: number;
     onPress: () => void;
 };
 
-const LeftContent = (props: { size: number }) => <View style={style.leftIcon}><Icon {...props} source="weather-sunny" size={20} /></View>;
+const LeftContent = (props: { iconName: string }) => <View style={style.leftIcon}><Icon {...props} source={props.iconName} size={20} /></View>;
 
 const CenterContent = (props: { cityName: string | undefined, weatherDescription: string | undefined }) => (
     <View style={style.middle}>
@@ -24,7 +25,7 @@ const CityCard: React.FC<Props> = ({ cityId, onPress }) => {
         <Card style={style.root} onPress={onPress}>
             {isPending || !data ? <ActivityIndicator /> :
                 <Card.Content style={style.content}>
-                    <LeftContent size={20} />
+                    <LeftContent iconName={mapIconsToCondition(data.weather[0].icon)} />
                     <CenterContent cityName={data?.name} weatherDescription={data?.weather[0]?.description} />
                     <Text variant="titleLarge">{Math.round(data?.main.temp)}℃</Text>
                 </Card.Content>
