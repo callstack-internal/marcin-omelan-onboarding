b4e9528cd3494ae6fca2e1ef437ff83880fd8e01
Leon Omelan <rozpierog@gmail.com>
2024-10-22 14:33:11 +0200

    change api call to return data in metric

--- src/api/CurrentWeather/index.ts
+++ src/api/CurrentWeather/index.ts
@@ -7,7 +7,7 @@ export const useCurrentWeather = (cityId: number) => {
     return useQuery<CurrentWeather>({
         queryKey: ['currentWeather', cityId], queryFn: async () => {
             const response = await fetch(
-                `https://api.openweathermap.org/data/2.5/weather?id=${cityId}&units;=metric&appid=${Config.OWM_API_KEY}`
+                `https://api.openweathermap.org/data/2.5/weather?id=${cityId}&units=metric&appid=${Config.OWM_API_KEY}`
             );
             return response.json();
         },
