6d30c0a370d37b8933a80538f8e0c61eadab2e78
Leon Omelan <rozpierog@gmail.com>
2024-10-23 09:40:32 +0200

    add basic data to the Details screen

--- src/screens/Details/index.tsx
+++ src/screens/Details/index.tsx
@@ -1,19 +1,26 @@
 
 import React from 'react';
 import { View } from 'react-native';
-import { useTheme, Text } from 'react-native-paper';
+import { useTheme, Text, ActivityIndicator } from 'react-native-paper';
 
 import style from './style';
 import type { RootStackScreenProps } from '../../schema/Navigation/types';
+import { useCurrentWeather } from '../../api/CurrentWeather';
 
 type Props = RootStackScreenProps<'Details'>;
 
 const Details: React.FC<Props> = ({ route }) => {
     const { cityId } = route.params;
+    const { isPending, data: currentData } = useCurrentWeather(cityId);
     const theme = useTheme();
     return (
         <View style={[style.root, { backgroundColor: theme.colors.surface }]}>
-            <Text>`${cityId} details\`</Text>
+            {isPending || !currentData ? <ActivityIndicator /> :
+                (<>
+                    <Text variant="titleSmall">{currentData.weather[0].description}</Text>
+                    <Text variant="displayLarge">{Math.round(currentData.main.temp)}℃</Text>
+                    <Text variant="bodySmall">{currentData.main.pressure}hPa</Text>
+                </>)}
         </View>
     );
 };
