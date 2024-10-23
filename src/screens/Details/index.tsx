411f995ebc5c3a826d3af7bd57d7a1de11390dc0
Leon Omelan <rozpierog@gmail.com>
2024-10-23 09:20:10 +0200

    Add background color to details page stub

--- src/screens/Details/index.tsx
+++ src/screens/Details/index.tsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import { useTheme, Text } from 'react-native-paper';
 
 import style from './style';
 import type { RootStackScreenProps } from '../../schema/Navigation/types';
@@ -9,8 +10,9 @@ type Props = RootStackScreenProps<'Details'>;
 
 const Details: React.FC<Props> = ({ route }) => {
     const { cityId } = route.params;
+    const theme = useTheme();
     return (
-        <View style={style.root}>
+        <View style={[style.root, { backgroundColor: theme.colors.surface }]}>
             <Text>`${cityId} details\`</Text>
         </View>
     );
