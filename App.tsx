af64d667af41fed124cd310a63d2fbd33f66e176
Leon Omelan <rozpierog@gmail.com>
2024-10-23 11:16:44 +0200

    add Paper theme to NavigationContainer

--- App.tsx
+++ App.tsx
@@ -1,33 +1,44 @@
 import React from 'react';
 
-import { PaperProvider } from 'react-native-paper';
+import { useColorScheme } from 'react-native';
+import { PaperProvider, adaptNavigationTheme } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
+import {
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme,
+} from '@react-navigation/native';
 
 import ListSceen from './src/screens/List';
 import DetailsScreen from './src/screens/Details';
 import type { RootStackParamList } from './src/schema/Navigation/types';
 
+const { LightTheme, DarkTheme } = adaptNavigationTheme({
+  reactNavigationLight: NavigationDefaultTheme,
+  reactNavigationDark: NavigationDarkTheme,
+});
+
+const queryClient = new QueryClient();
+
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function App(): React.JSX.Element {
+  const colorScheme = useColorScheme();
   return (
     <QueryClientProvider client={queryClient}>
       <PaperProvider>
-        <NavigationContainer>
+        <NavigationContainer theme={colorScheme === 'dark' ? DarkTheme : LightTheme}>
           <Stack.Navigator initialRouteName="List">
             <Stack.Screen name="List" component={ListSceen} />
             <Stack.Screen name="Details" component={DetailsScreen} />
           </Stack.Navigator>
         </NavigationContainer>
       </PaperProvider>
-    </QueryClientProvider>
+    </QueryClientProvider >
   );
 }
 
