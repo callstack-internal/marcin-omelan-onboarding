e810c123a500bc6ddfe2114dd15eab2f0ef2a6c6
Leon Omelan <rozpierog@gmail.com>
2024-10-25 16:14:16 +0200

    Add retry mechanism to Android location getter

--- android/app/src/main/java/com/onboarding/modules/NativeLocationModule.kt
+++ android/app/src/main/java/com/onboarding/modules/NativeLocationModule.kt
@@ -1,21 +1,17 @@
 package com.onboarding.modules
 
 import android.Manifest
+import android.Manifest.permission
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.LocationManager
-import android.location.LocationRequest
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity
+import androidx.annotation.RequiresPermission
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.content.PackageManagerCompat
 import androidx.core.location.LocationManagerCompat
-import androidx.core.location.LocationRequestCompat
 import com.facebook.react.bridge.Arguments
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.WritableMap
 import com.onboarding.NativeLocationSpec
 
 
@@ -37,23 +33,36 @@ class NativeLocationModule(val reactContext: ReactApplicationContext) :
         } else locationManager.allProviders.first()
         if (ActivityCompat.checkSelfPermission(
                 reactContext,
-                Manifest.permission.ACCESS_FINE_LOCATION
+                permission.ACCESS_FINE_LOCATION
             ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
                 reactContext,
-                Manifest.permission.ACCESS_COARSE_LOCATION
+                permission.ACCESS_COARSE_LOCATION
             ) != PackageManager.PERMISSION_GRANTED
         ) {
             return
         }
-        LocationManagerCompat.getCurrentLocation(locationManager,
-            provider,
-            null,
-            ContextCompat.getMainExecutor(reactContext),
-        ) { location ->
-            promise.resolve(Arguments.createMap().apply {
-                putDouble("longitude", location.longitude)
-                putDouble("latitude", location.latitude)
-            })
+        var retryCount = 0
+        fun getLocation() {
+            LocationManagerCompat.getCurrentLocation(
+                locationManager,
+                provider,
+                null,
+                ContextCompat.getMainExecutor(reactContext),
+            ) { location ->
+                if (location == null) {
+                    if (retryCount < 3) {
+                        retryCount += 1
+                        getLocation()
+                        return@getCurrentLocation
+                    }
+                }
+                promise.resolve(Arguments.createMap().apply {
+                    putDouble("longitude", location.longitude)
+                    putDouble("latitude", location.latitude)
+                })
+            }
         }
+        getLocation()
     }
 }
+
