862737f42ebd384efaedb9de7eafefa1d7ab9136
Leon Omelan <rozpierog@gmail.com>
2024-10-25 15:45:57 +0200

    rewrite nativelocation on iOS to provide current location of the user

--- ios/NativeLocation/RCTNativeLocation.mm
+++ ios/NativeLocation/RCTNativeLocation.mm
@@ -4,50 +4,47 @@
 
 @interface RCTNativeLocation() <CLLocationManagerDelegate>
 @property (nonatomic, strong) CLLocationManager *locationManager;
-@property (nonatomic, copy) RCTPromiseResolveBlock resolveBlock;
-@property (nonatomic, copy) RCTPromiseRejectBlock rejectBlock;
+@property (nonatomic, copy)  CLLocation *currentLocation;;
 @end
 @implementation RCTNativeLocation
 
 RCT_EXPORT_MODULE(NativeLocation)
 
-- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:(const facebook::react::ObjCTurboModule::InitParams &)params { 
+
+
+- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:(const facebook::react::ObjCTurboModule::InitParams &)params {
   return std::make_shared<facebook::react::NativeLocationSpecJSI>(params);
 }
 
-- (void)getLocation:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject { 
-  self.resolveBlock = resolve;
-  self.rejectBlock = reject;
-  
-  self.locationManager = [[CLLocationManager alloc] init];
-  self.locationManager.delegate = self;
-  [self.locationManager requestWhenInUseAuthorization];
-  if ([CLLocationManager locationServicesEnabled]) {
-    [self.locationManager requestLocation];
-  } else {
-      self.rejectBlock(@"location_services_disabled", @"Location services are disabled", nil);
-  }
+- (void)getLocation:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
+  @try {
+          CLLocationManager *locationManager = [[CLLocationManager alloc] init];
+          locationManager.delegate = self;
+          locationManager.distanceFilter = kCLDistanceFilterNone;
+          locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+          [locationManager requestWhenInUseAuthorization];
+          [locationManager startUpdatingLocation];
+          
+          // Get coordinate data
+          CLLocation *location = [locationManager location];
+          CLLocationCoordinate2D coordinate = [location coordinate];
+          
+          // Get latitude, longitude by string
+          NSString *latitude = [NSString stringWithFormat:@"%f", coordinate.latitude];
+          NSString *longitude = [NSString stringWithFormat:@"%f", coordinate.longitude];
+          
+          // Return an array value of latitude, longitude to JS code
+          NSArray *result = @[latitude, longitude];
+          resolve(result);
+      } @catch (NSException *exception) {
+          reject(@"Error description", @"Can not get the current location", nil);
+      };
 }
 
-#pragma mark - CLLocationManagerDelegate
-
-// TODO: didUpdateLocations isn`t called for some reason
-- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations {
-    CLLocation *location = [locations lastObject];
-    if (location) {
-        NSDictionary *locationData = @{
-            @"longitude": @(location.coordinate.longitude),
-            @"latitude": @(location.coordinate.latitude)
-        };
-        self.resolveBlock(locationData);
-    } else {
-        self.rejectBlock(@"no_location", @"Could not fetch location", nil);
-    }
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
+{
+    self.currentLocation = [locations lastObject];
     [self.locationManager stopUpdatingLocation];
-}
-
-- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
-    self.rejectBlock(@"location_error", error.localizedDescription, error);
 }
 
 @end
