2eec931d6d8fae20de459a118689762c97f80fe3
Leon Omelan <rozpierog@gmail.com>
2024-10-29 14:34:43 +0100

    answer yes to maestroSetup in CI

--- bin/maestroSetup.sh
+++ bin/maestroSetup.sh
@@ -2,18 +2,18 @@
 
 if ! [ -x "$(command -v maestro)" ]; then
     echo 'Error: maestro is not installed.' >&2
-    read -p "Do you want to install maestro? (y/n)? " answer
-    case ${answer:0:1} in
-        y|Y )
-            if ! [ -x "$(command -v brew)" ]; then
-                brew tap mobile-dev-inc/tap
-                brew install maestro
-            else
-                curl -fsSL "https://get.maestro.mobile.dev" | bash
-            fi
+    read -p -r "Do you want to install maestro? (y/n)?" answer
+    case ${answer} in
+    y | Y)
+        if ! [ -x "$(command -v brew)" ]; then
+            brew tap mobile-dev-inc/tap
+            brew install maestro
+        else
+            curl -fsSL "https://get.maestro.mobile.dev" | bash
+        fi
         ;;
-        * )
-            exit 1
+    *)
+        exit 1
         ;;
     esac
 fi
@@ -21,18 +21,18 @@ echo '✅ maestro is installed.'
 
 if ! [ -x "$(command -v flashlight)" ]; then
     echo 'Error: flashlight is not installed.' >&2
-    read -p "Do you want to install flashlight? (y/n)? " answer
-    case ${answer:0:1} in
-        y|Y )
-             curl https://get.flashlight.dev | bash
-            # check if arch is arm64 if yes install rosetta
-            if [ "$(uname -m)" == "arm64" ] && [[ "$OSTYPE" == "darwin"* ]]; then
-                echo "🚀 Installing Rosetta"
-                softwareupdate --install-rosetta --agree-to-license
-            fi
+    read -p -r "Do you want to install flashlight? (y/n)?" answer
+    case ${answer} in
+    y | Y)
+        curl https://get.flashlight.dev | bash
+        # check if arch is arm64 if yes install rosetta
+        if [ "$(uname -m)" == "arm64" ] && [[ "$OSTYPE" == "darwin"* ]]; then
+            echo "🚀 Installing Rosetta"
+            softwareupdate --install-rosetta --agree-to-license
+        fi
         ;;
-        * )
-            exit 1
+    *)
+        exit 1
         ;;
     esac
 fi
