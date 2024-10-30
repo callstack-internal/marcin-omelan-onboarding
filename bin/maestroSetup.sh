1b20b5bc9b31fb78a9fb669cdb3945473758d3e1
Leon Omelan <rozpierog@gmail.com>
2024-10-30 13:34:58 +0100

    setup maestro performance testing and reports on CI (#10)
    * setup maestro performance testing and reports on CI
    
    * add apk to artifacts

---
+++ bin/maestroSetup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if ! [ -x "$(command -v maestro)" ]; then
+    echo 'Error: maestro is not installed.' >&2
+    read -p "Do you want to install maestro? (y/n)?" answer
+    case ${answer} in
+    y | Y)
+        if ! [ -x "$(command -v brew)" ]; then
+            curl -fsSL "https://get.maestro.mobile.dev" | bash
+        else
+            brew tap mobile-dev-inc/tap
+            brew install maestro
+        fi
+        ;;
+    *)
+        exit 1
+        ;;
+    esac
+fi
+echo '✅ maestro is installed.'
+
+if ! [ -x "$(command -v flashlight)" ]; then
+    echo 'Error: flashlight is not installed.' >&2
+    read -p "Do you want to install flashlight? (y/n)?" answer
+    case ${answer} in
+    y | Y)
+        curl https://get.flashlight.dev | bash
+        # check if arch is arm64 if yes install rosetta
+        if [ "$(uname -m)" == "arm64" ] && [[ "$OSTYPE" == "darwin"* ]]; then
+            echo "🚀 Installing Rosetta"
+            softwareupdate --install-rosetta --agree-to-license
+        fi
+        ;;
+    *)
+        exit 1
+        ;;
+    esac
+fi
+echo '✅ flashlight is installed.'
