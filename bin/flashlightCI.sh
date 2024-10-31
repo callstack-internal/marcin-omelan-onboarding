0f3cfb94cb13bb76e1e8e12e9802ca6f9893f008
Leon Omelan <rozpierog@gmail.com>
2024-10-31 10:21:56 +0100

    simplify Ci invocation of the maestro + flashlight. Also attach maestro reports

--- bin/flashlightCI.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# To be used on CI server
-cd android || exit
-./gradlew installRelease
-cd ..
-"$HOME"/.flashlight/bin/flashlight test --bundleId com.onboarding --testCommand "$HOME/.maestro/bin/maestro test __tests__/maestro/NavigationAndroid.yaml" --duration 10000 --resultsFilePath results.json
